fa7ab7bd04bbd6332387e84de1482fb540ad5217
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2025-05-11 11:40:09 +0200

    update vercel.json & wsgi

--- core/settings.py
+++ core/settings.py
@@ -143,14 +143,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+
+STATIC_URL = '/static/'
+STATIC_ROOT  = BASE_DIR / 'staticfiles_build'
 
 
-# Configuration des médias
+
+# # Configuration des médias
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
--- core/wsgi.py
+++ core/wsgi.py
@@ -7,12 +7,30 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# application = get_wsgi_application()
+
+# app = application
+
+
+import os
+import django
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+django.setup()
 
-application = get_wsgi_application()
+# Automatically run migrations and collectstatic on deployment (optional)
+from django.core.management import call_command
+try:
+    call_command("migrate")
+    call_command("collectstatic", "--noinput")
+except Exception as e:
+    print("Migration/static error:", e)
 
-app = application
+app = get_wsgi_application()
